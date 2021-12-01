2d94b27e7dcae464d7f9e91e96b41406e64f720b
mahosAtpug <68770124+mahosAtpug@users.noreply.github.com>
2021-12-01 18:49:43 +0530

    Add files via upload

--- scraperhw.py
+++ scraperhw.py
@@ -14,23 +14,21 @@ def scrape():
     star_data = []
     for i in range(0 , 458): 
         soup = BeautifulSoup(browser.page_source , "html.parser")
-        for ul_tag in soup.find_all("ul" , attrs={"class" , "stars"}):
-            li_tags = ul_tag.find_all("li")
+        for tr_tag in soup.find_all("tr" , attrs={"class" , "stars"}):
+            td_tags = tr_tag.find_all("td")
             temp_list = []
-            # iterate over the li tags found in 15 line
 
-            for index , li_tag in enumerate(li_tags):
+            for index , td_tag in enumerate(td_tags):
                 if index== 0:
-                    temp_list.append(li_tag.find_all("a")[0].contents[0])
+                    temp_list.append(td_tag.find_all("a")[0].contents[0])
                 else:
                     try:
-                        temp_list.append(li_tag.contents[0])
+                        temp_list.append(td_tag.contents[0])
                     except:
                         temp_list.append("")
 
             star_data.append(temp_list)
     browser.find_element_by_xpath('//*[@id="mw-content-text"]/div[1]/table/tbody/tr[1]/td[2]/a').click()
-    # xpath -> syntax to describe parts of xml documents
     with open("stars.csv" , "w" )as f:
         csvwriter = csv.writer(f)
         csvwriter.writerow(headers)
